de167787231010490010c7054b5c3952595f4c11
matheusangelo <matheusangelo10@gmail.com>
2019-08-10 15:20:12 -0300

    Ajuste CSS login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBInputGroup } from 'mdbreact';
+import React, { Component, useState, useEffect } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBInputGroup, MDBCard, MDBCardBody } from 'mdbreact';
 import { Button, Form } from 'reactstrap';
 import './login.css';
 import NavbarTopo from '../navbar/navbar'
 
 export default function Login() {
+    // const [email, setEmail] = useState('');
+    // const [senha,setSenha] = useState('');
+
     return (
         <>
             <NavbarTopo />
@@ -13,31 +16,34 @@ export default function Login() {
                     <MDBCol>
                         <div className="grey-text">
                             <Form className="login-form">
-                                <p className="h5 text-center"> Sign-In</p>
-
-                                <MDBInput
-                                    icon="envelope"
-                                    label="E-mail"
-                                    group
-                                    type="email"
-                                    validate
-                                    error="wrong"
-                                    success="right"
-                                    containerClass="text-left"
-                                />
-                                <MDBInput
-                                    icon="lock"
-                                    label="Password"
-                                    group
-                                    type="password"
-                                    validate
-                                    containerClass="text-left"
-                                />
-                                <hr />
-                                <div className="text-center">
-                                    <Button type="submit" className="login-btn mb-4" color="primary">Entrar</Button>
-                                    <Button type="button" className="login-btn mb-4" color="danger">Cadastrar-se</Button>
-                                </div>
+                                <MDBCard >
+                                    <MDBCardBody>
+                                        <p className="h5 text-center"> Sign-In</p>
+                                        <MDBInput
+                                            icon="envelope"
+                                            label="E-mail"
+                                            group
+                                            type="email"
+                                            validate
+                                            error="wrong"
+                                            success="right"
+                                            containerClass="text-left"
+                                        />
+                                        <MDBInput
+                                            icon="lock"
+                                            label="Password"
+                                            group
+                                            type="password"
+                                            validate
+                                            containerClass="text-left"
+                                        />
+                                        <hr />
+                                        <div className="text-center">
+                                            <Button type="submit" className="login-btn mb-4" color="primary">Entrar</Button>
+                                            <Button type="button" className="login-btn mb-4" color="danger">Cadastrar-se</Button>
+                                        </div>
+                                    </MDBCardBody>
+                                </MDBCard>
                             </Form>
                         </div>
                     </MDBCol>
